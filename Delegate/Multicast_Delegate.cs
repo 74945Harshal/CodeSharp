9a79c4ce8e9676ee543d3694f93b064661b74915
Harshal_Sarode <69304097+74945Harshal@users.noreply.github.com>
2023-11-03 01:25:40 +0530

    Add files via upload

---
+++ Delegate/Multicast_Delegate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+   delegate void MyMath(int a, int b);
+    class Multicast
+    {
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine("Addition is "+ (a + b));
+        }
+        public static void Product(int a, int b)
+        {
+            Console.WriteLine("Product is "+(a * b));
+        }
+    }
+
+    internal class Multicast_Delegate
+    {
+        static void Main(string[] args)
+        {
+            MyMath m1=null;
+            MyMath ad = Multicast.Add;
+            MyMath pd= Multicast.Product;
+
+            //set up multicast
+            m1 += ad; 
+            m1 += pd;
+
+            //call multicast
+            m1(5, 6);
+
+            //remove method from Invokation list
+            m1 -= ad;
+
+            //call multicast
+            m1(10, 20);
+        }
+    }
+}
